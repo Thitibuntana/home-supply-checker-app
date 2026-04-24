ea81dad12ca8de7158256caab8915f5898fe7a2b
Thitibuntana T <s6652410022@sau.ac.th>
2026-04-24 19:21:59 +0700

    Fix Vercel SSR window not defined bug

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,16 +1,35 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native';
 import 'react-native-url-polyfill/auto';
 import { Database } from './database.types';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
+const isWeb = Platform.OS === 'web';
+const isSSR = typeof window === 'undefined';
+
+const customStorage = {
+  getItem: async (key: string) => {
+    if (isSSR) return null;
+    return await AsyncStorage.getItem(key);
+  },
+  setItem: async (key: string, value: string) => {
+    if (isSSR) return;
+    await AsyncStorage.setItem(key, value);
+  },
+  removeItem: async (key: string) => {
+    if (isSSR) return;
+    await AsyncStorage.removeItem(key);
+  },
+};
+
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: customStorage,
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false,
+    detectSessionInUrl: isWeb,
   },
 });
